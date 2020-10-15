2e98ee2190f2acf3500f429b773c3372eaa541bd
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-15 17:04:23 +0530

    API Server setup & Hello World

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
+  /* Database Server Connect */
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  /* const post = orm.em.create(Post, {
-    title: "My First Post",
+
+  /* API Server */
+  const app = express();
+  const port = 4000;
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
-  await orm.em.persistAndFlush(post); */
-  const posts = await orm.em.find(Post, {});
-  console.table(posts);
+  server.applyMiddleware({ app });
+  app.get("/", (_req, res) => {
+    res.send("Hello World!");
+  });
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
 };
 
 main();
