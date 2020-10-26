aff07b8db241b7e665aa7507576d728177743391
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-27 00:33:17 +0530

    Update cookie domain

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ const main = async () => {
         httpOnly: true,
         sameSite: "lax",
         secure: __prod__,
-        domain: __prod__ ? ".web-social-news-app.vercel.app" : undefined,
+        domain: __prod__ ? ".harrymanchanda.in" : undefined,
       },
       saveUninitialized: false,
       secret: SESSION_SECRET as string,
